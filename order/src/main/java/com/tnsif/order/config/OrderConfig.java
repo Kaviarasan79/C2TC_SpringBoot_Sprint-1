69a33dd605f7e7d8a9dce3e101926f6e19810d4e
Kaviarasan79 <kaviarasan0709@gmail.com>
2025-10-19 10:44:07 +0530

    final done

--- order/src/main/java/com/tnsif/order/config/OrderConfig.java
+++ order/src/main/java/com/tnsif/order/config/OrderConfig.java
@@ -1,18 +1,16 @@
 package com.tnsif.order.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class OrderConfig implements WebMvcConfigurer {
-	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/")
-			.allowedOrigins("http://localhost:3000/")
-			.allowedMethods("GET", "POST", "PUT", "DELETE");
-	}
 
+public class OrderConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/orderservice/**")                    // <-- allow all endpoints
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 }
--- order/src/main/java/com/tnsif/order/controller/OrderController.java
+++ order/src/main/java/com/tnsif/order/controller/OrderController.java
@@ -19,7 +19,9 @@ import jakarta.persistence.NoResultException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000") // React frontend
 @RestController
 public class OrderController {
     
